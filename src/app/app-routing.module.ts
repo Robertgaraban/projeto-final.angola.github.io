6e153059a68972c34478a4c552a9aa02d3f305c6
Robertgaraban <robertgaraban@gmail.com>
2020-08-25 01:14:36 -0300

    Paginas Home e Busqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent} from "./components/home/home.component";
+import { BusquedaComponent} from "./components/busqueda/busqueda.component";
+
+
+const routes: Routes = [
+
+  
+  { path: "Home", component: HomeComponent },
+  { path: "Busqueda", component: BusquedaComponent },
+  { path: "**", pathMatch: 'full', redirectTo: 'Home' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
